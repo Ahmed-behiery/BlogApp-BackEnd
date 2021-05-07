0fb1a48f3843d3b5c1ae0b06b0662cd95133f522
Ahmed-Behery <ahmedbehiery96@gmail.com>
2021-05-08 01:07:22 +0200

    jfj

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 var bodyParser = require("body-parser");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const cloudinary = require("cloudinary").v2;
 
